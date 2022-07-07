693642150272bb489324de5f3a2cc338bde6ea01
99010912Tamicktom@ <rickpokemon38@gmail.com>
2022-07-06 23:40:07 -0300

    fim do dia

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Este é o repositório do front-end do e-comerce No Sigilo.
+Este é o repositório do servidor front-end do e-comerce No Sigilo.
+
+O projeto ainda está em estágios iniciais de desenvolvimento (tanto que apenas a versão responsividade para a versão mobile foi feita até agora :3).
+
+Para desenvolvimento está sendo utilizado NextJs, SASS e uma API feita em PHP para o back-end (onde serão armazenados os dados dos usuários).
+
+Não há muito o que ser visto aqui, até porque as coisas ainda estão sendo constantemente alteradas e movidas de lugar.
--- pages/produto/index.tsx
+++ pages/produto/index.tsx
@@ -5,12 +5,22 @@ import GlobalHead from '../../components/GlobalHead/GlobalHead';
 type Props = {}
 
 export default function index({ }: Props) {
+
     return (
         <div>
             <GlobalHead />
             <NavBar />
-            <main>
-                
+            <main style={{ color: 'white', marginTop: '75px' }}>
+
+                <p>MARCA DO PRODUTO</p>
+                <p>Código: <span>xxxxxx</span></p>
+                <p><span>Nome do Produto</span> - Descrição Curta, Descrição Curta, Descrição Curta, Descrição Curta, Descrição Curta.</p>
+
+                {/* parte com as fotos do produto */}
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
             </main>
         </div>
     )
